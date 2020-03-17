0ce08318a0545cd23514afe747ce395bd1c2a644
Abhishek Sinha <avishkarabhishek786@gmail.com>
2020-03-17 22:47:31 +0530

    changed architecture, updated deposit withdraw logic

--- supernode/server/chatRoomWSServer.c
+++ supernode/server/chatRoomWSServer.c
@@ -6,7 +6,7 @@
 #include "mongoose.h"
 
 static sig_atomic_t s_signal_received = 0;
-static const char *s_http_port = "9003";
+static const char *s_http_port = "9115";
 static struct mg_serve_http_opts s_http_server_opts;
 
 static void signal_handler(int sig_num) {
@@ -20,16 +20,21 @@ static int is_websocket(const struct mg_connection *nc) {
 
 static void broadcast(struct mg_connection *nc, const struct mg_str msg) {
   struct mg_connection *c;
-  char buf[25000];
-  char addr[10240];
-  mg_sock_addr_to_str(&nc->sa, addr, sizeof(addr),
-                      MG_SOCK_STRINGIFY_IP | MG_SOCK_STRINGIFY_PORT);
-
-  snprintf(buf, sizeof(buf), "%s %.*s", addr, (int) msg.len, msg.p);
-  printf("%s\n", buf); /* Local echo. */
+  printf("(Broadcast) \t[%d]\n", (int)msg.len);
   for (c = mg_next(nc->mgr, NULL); c != NULL; c = mg_next(nc->mgr, c)) {
     if (c == nc) continue; /* Don't send to the sender. */
-    mg_send_websocket_frame(c, WEBSOCKET_OP_TEXT, buf, strlen(buf)); // A single websocket frame is sent
+    mg_send_websocket_frame(c, WEBSOCKET_OP_TEXT, msg.p, (int)msg.len);
+  }
+}
+
+void unicast(struct mg_connection *nc, const struct mg_str msg) {
+  struct mg_connection *c;
+  char recipient_floID[35];
+  snprintf(recipient_floID, sizeof(recipient_floID), "%.*s", 34, &msg.p[1]);
+  printf("%s \t[%d]\n",recipient_floID, (int)msg.len - 36); /* Local echo. */
+  for (c = mg_next(nc->mgr, NULL); c != NULL; c = mg_next(nc->mgr, c)) {
+    if (!strcmp(c->floID,recipient_floID))  /* Send to receiver */
+      mg_send_websocket_frame(c, WEBSOCKET_OP_TEXT, &msg.p[36], (int)msg.len - 36);
   }
 }
 
@@ -37,14 +42,18 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
   switch (ev) {
     case MG_EV_WEBSOCKET_HANDSHAKE_DONE: {
       /* New websocket connection. Tell everybody. */
-      broadcast(nc, mg_mk_str("++ joined"));
       break;
     }
     case MG_EV_WEBSOCKET_FRAME: {
       struct websocket_message *wm = (struct websocket_message *) ev_data;
       /* New websocket message. Tell everybody. */
-      struct mg_str d = {(char *) wm->data, wm->size}; //This is the data received data inside *ev_data
-      broadcast(nc, d); //Action when data is received
+      struct mg_str d = {(char *) wm->data, wm->size};
+      if (d.p[0] == '$')
+        snprintf(nc->floID, sizeof(nc->floID), "%.*s", 34, &d.p[1]);
+      else if ((d.p[0] == '>'))
+        unicast(nc, d);
+      else
+        broadcast(nc, d);
       break;
     }
     case MG_EV_HTTP_REQUEST: {
@@ -54,7 +63,7 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
     case MG_EV_CLOSE: {
       /* Disconnect. Tell everybody. */
       if (is_websocket(nc)) {
-        broadcast(nc, mg_mk_str("-- left"));
+        //broadcast(nc, mg_mk_str("-- left"));
       }
       break;
     }
@@ -72,10 +81,10 @@ int main(void) {
 
   mg_mgr_init(&mgr, NULL);
 
-  nc = mg_bind(&mgr, s_http_port, ev_handler); //connection mg_connection and manager mg_mgr are linked along with port and event handler
+  nc = mg_bind(&mgr, s_http_port, ev_handler);
   mg_set_protocol_http_websocket(nc);
-  s_http_server_opts.document_root = ".";  // Serve current directory
-  s_http_server_opts.enable_directory_listing = "yes";
+  s_http_server_opts.document_root = "web";  // Serve current directory
+  s_http_server_opts.enable_directory_listing = "no";
 
   printf("Started on port %s\n", s_http_port);
   while (s_signal_received == 0) {
